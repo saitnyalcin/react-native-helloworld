1a236a87be9a1424b776138b6f62337fd1ae6e55
Steve Yalcin <steveyalcin@Steves-MacBook-Pro.local>
2022-02-11 19:48:51 -0500

    added some native core components into the view

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+  const [name, setname] = useState("steve");
+  const [age, setage] = useState("30");
+
   return (
     <View style={styles.container}>
-      <Text>Hello World application!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Enter your name..."
+        onChangeText={(name) => setname(name)}
+      />
+      <Text>Name: {name}</Text>
+
+      <TextInput
+        keyboardType="numeric"
+        style={styles.inputStyle}
+        placeholder="Enter your age..."
+        onChangeText={(age) => setage(age)}
+      />
+      <Text>Age: {age}</Text>
     </View>
   );
 }
@@ -13,8 +28,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  inputStyle: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
